cffc89e1cf6facf6405efd0248da064ddbfd264e
SGcodework <sgcodework@gmail.com>
2024-06-27 17:59:12 +0530

    add

--- src/componets/home/index.jsx
+++ src/componets/home/index.jsx
@@ -1,5 +1,7 @@
 import React from "react";
 
 export default function Home() {
-  return <div>Hello Zeel to Shruti yash a sikhvadu amne 3 ne</div>;
+  return (
+    <div>Hello Zeel to Shruti yash a sikhvadu amne 3 ne hihihihiiiiiiiiiii</div>
+  );
 }
